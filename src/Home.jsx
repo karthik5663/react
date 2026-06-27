bf1f6707567656b3c4145fd8e22d54c4432f8423
karthik <karthik@gmail.com>
2026-06-27 20:46:27 +0530

    v1

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
 export default function Home() {
+  const db = import .meta.env.VITE_DB_URL || 'no url is there'
   return (
     <div>
       <a href="/about">about page</a> <br />
-      <a href="/contact">contact page</a>
+      <a href="/contact">contact page</a><br /><br />
+       value : {db}
     </div>
   )
 }
